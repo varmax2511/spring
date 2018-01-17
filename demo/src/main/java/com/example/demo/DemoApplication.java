e7ac23eb5d22d8b6901dba9d447485abf46d1ff7
varmax2511 <var_max@hotmail.com>
2018-01-16 23:42:35 -0500

    - Basic authentication

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-//@EnableOAuth2Sso
-/*
- * @EnableAutoConfiguration(exclude = { /
- * org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.
- * class })
- */
 public class DemoApplication {
 
   public static void main(String[] args) {
